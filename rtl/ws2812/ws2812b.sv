ba625aabe8d0f863ab81426abda626c8b3c120d2
Kenta IDA <fuga@fugafuga.org>
2021-09-04 20:42:15 +0900

    Add WS2812 example for GOWIN Runber

--- rtl/ws2812/ws2812b.sv
+++ rtl/ws2812/ws2812b.sv
@@ -2,29 +2,27 @@
  * @file ws2812b.sv
  * @brief control signal generator for Worldsemi WS2812B and variants.
  */
-// Copyright 2019 Kenta IDA
+// Copyright 2021 Kenta IDA
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 
 module ws2812b #(
-    parameter longint CLOCK_HZ = 24_000_000,
-    parameter NUMBER_OF_LEDS = 16,
-    localparam NUMBER_OF_REGS = 4,
-    localparam REG_ADDRESS_WIDTH = $clog2(NUMBER_OF_REGS)
+    parameter longint CLOCK_HZ = 12_000_000,
+    parameter NUMBER_OF_LEDS = 16
 ) (
     input  logic clock,
-    input  logic resetn,
+    input  logic reset,
 
     output logic serial_out,
 
-    input  logic [REG_ADDRESS_WIDTH-1:0] reg_address,
-    input  logic                         reg_is_write,
-    input  logic                         reg_request,
-    output logic                         reg_response,
-    output logic [7:0]                   reg_read_data,
-    input  logic [7:0]                   reg_write_data
+    // LED pixel buffer interface
+    output logic [$clog2(NUMBER_OF_LEDS*3)-1:0] pixel_address,
+    output logic                                pixel_request,
+    input  logic                                pixel_ready,
+    input  logic                                pixel_valid,
+    input  logic [7:0]                          pixel_data
 );
 
 localparam longint LONG_PERIOD  = (580*CLOCK_HZ + 1000_000_000-1)/1000_000_000;
@@ -36,51 +34,75 @@ typedef struct packed {
     bit [7:0] r;
     bit [7:0] b;
 } pixel_t;
-
-pixel_t pixels[NUMBER_OF_LEDS-1:0]; /* synthesis syn_ramstyle="block_ram" */
-
 typedef bit [$clog2(NUMBER_OF_LEDS) - 1:0] led_index_t;
-led_index_t led_index;
-pixel_t  led_pixel_buffer;
-localparam REG_COMMAND = 0;
-localparam REG_B       = 1;
-localparam REG_G       = 2;
-localparam REG_R       = 3;
 
-always @(posedge clock) begin
-    if( !resetn ) begin
-        led_index <= 0;
+// Read pixel data process
+pixel_t     pixel_buffer_next = 0;          // The next pixel data
+logic       pixel_buffer_next_valid = 0;    // Is the next pixel data valid?
+logic       pixel_buffer_next_ready = 0;    // Is the next pixel data ready to consume?
+led_index_t pixel_counter = 0;              // Pixel index counter. [0 .. NUMBER_OF_LEDS-1]
+logic [1:0] pixel_color_counter = 0;        // Pixel color counter. [0 .. 2]
+logic [1:0] pixel_address_counter = 0;      // Pixel address counter. [0 .. 2]
+typedef enum { S_READ_PIXEL_IDLE, S_READ_PIXEL_READ } read_state_t;
+read_state_t pixel_read_state = S_READ_PIXEL_IDLE;
+
+always_ff @(posedge clock) begin
+    if( reset ) begin
+        pixel_read_state <= S_READ_PIXEL_IDLE;
+        pixel_buffer_next <= 0;
+        pixel_counter <= 0;
+        pixel_color_counter <= 0;
+        pixel_address <= 0;
+        pixel_address_counter <= 0;
+        pixel_request <= 0;
     end
     else begin
-        reg_response <= 0;
-        if( reg_request ) begin
-            if( reg_is_write ) begin
-                case(reg_address)
-                REG_COMMAND: begin
-                    led_index <= reg_write_data;
-                    led_pixel_buffer <= pixels[led_index];
-                end
-                REG_R: begin led_pixel_buffer.r <= reg_write_data; pixels[led_index] <= '{r: reg_write_data, g: led_pixel_buffer.g, b: led_pixel_buffer.b}; end
-                REG_G: begin led_pixel_buffer.g <= reg_write_data; pixels[led_index] <= '{r: led_pixel_buffer.r, g: reg_write_data, b: led_pixel_buffer.b}; end
-                REG_B: begin led_pixel_buffer.b <= reg_write_data; pixels[led_index] <= '{r: led_pixel_buffer.r, g: led_pixel_buffer.g, b: reg_write_data}; end
-                default: begin
-                    led_index <= led_index;
-                    led_pixel_buffer <= led_pixel_buffer;
-                end
-                endcase
-            end
-            else begin
-                case(reg_address)
-                REG_COMMAND: begin
-                    reg_read_data <= led_index;
-                end
-                REG_R: reg_read_data <= led_pixel_buffer.r;
-                REG_G: reg_read_data <= led_pixel_buffer.g;
-                REG_B: reg_read_data <= led_pixel_buffer.b;
-                endcase
-            end
-            reg_response <= 1;
+        if( pixel_buffer_next_valid && pixel_buffer_next_ready ) begin
+            pixel_buffer_next_valid <= 0;   // Clear valid if the pixel data in the pixel_buffer_next is consumed.
         end
+
+        case(pixel_read_state)
+            S_READ_PIXEL_IDLE: begin
+                // Check if the pixel_buffer_next is empty or ready to consume.
+                if( !pixel_buffer_next_valid || pixel_buffer_next_ready ) begin
+                    // Begin to read the next pixel data.
+                    pixel_color_counter <= 0;
+                    pixel_address_counter <= 0;
+                    pixel_request <= 1;
+                    pixel_read_state <= S_READ_PIXEL_READ;
+                end
+            end
+            S_READ_PIXEL_READ: begin
+                if( pixel_request && pixel_ready ) begin
+                    pixel_address <= pixel_address < NUMBER_OF_LEDS*3 - 1 ? pixel_address + 1 : 0;
+                    if( pixel_address_counter < 2 ) begin
+                        pixel_address_counter <= pixel_address_counter + 1;
+                    end 
+                    else begin
+                        pixel_request <= 0;
+                    end
+                end
+                if( pixel_valid ) begin
+                    // Reorder the pixel data. (BGR888 -> GRB888)
+                    case(pixel_color_counter)
+                        0: pixel_buffer_next.b <= pixel_data;
+                        1: pixel_buffer_next.g <= pixel_data;
+                        2: pixel_buffer_next.r <= pixel_data;
+                    endcase
+
+                    if( pixel_color_counter < 2 ) begin
+                        pixel_color_counter <= pixel_color_counter + 1;
+                    end
+                    else begin
+                        pixel_buffer_next_valid <= 1;
+                        pixel_color_counter <= 0;
+                        pixel_read_state <= S_READ_PIXEL_IDLE;
+                        pixel_counter <= pixel_counter < NUMBER_OF_LEDS - 1 ? pixel_counter + 1 : 0;
+                    end
+                end
+            end
+        default: pixel_read_state <= S_READ_PIXEL_IDLE;
+        endcase
     end
 end
 
@@ -97,27 +119,35 @@ state_t state;
 
 always_comb begin
     case(state)
-    S_RESET: serial_out = 0;
-    S_BIT_LOW: serial_out = 0;
-    default: serial_out = 1;
+        S_RESET: serial_out = 0;
+        S_BIT_LOW: serial_out = 0;
+        default: serial_out = 1;
+    endcase
+end
+
+always_comb begin
+    case(state)
+        S_BEGIN_LED: pixel_buffer_next_ready = 1;
+        default: pixel_buffer_next_ready = 0;
     endcase
 end
 
 always_ff @(posedge clock) begin
-    if( !resetn ) begin
-        state <= IDLE;
-        pixel <= 0;
+    if( reset ) begin
+        state <= S_IDLE;
     end
     else begin
         case(state)
         S_IDLE: begin
             period_counter <= LONG_PERIOD+SHORT_PERIOD;
             led_counter <= 0;
+            pixel <= 0;
             state <= S_INTERVAL;
         end
         S_INTERVAL: begin
             period_counter <= period_counter - 1;
             if( period_counter == 0 ) begin
+                period_counter <= RESET_PERIOD;
                 state <= S_RESET;
             end
         end
@@ -129,8 +159,10 @@ always_ff @(posedge clock) begin
         end
         S_BEGIN_LED: begin
             bit_counter <= 23;
-            pixel <= pixels[led_counter];
-            state <= S_BEGIN_BIT;
+            pixel <= pixel_buffer_next;
+            if( pixel_buffer_next_valid ) begin
+                state <= S_BEGIN_BIT;
+            end
         end
         S_BEGIN_BIT: begin
             period_counter <= pixel[bit_counter] ? LONG_PERIOD : SHORT_PERIOD;
