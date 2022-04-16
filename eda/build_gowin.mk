9ed0d143a474ba5f96bc5d336006ff37132277c5
Kenta IDA <fuga@fugafuga.org>
2022-04-17 05:10:01 +0900

    Add TangNano1k port of blink.

--- eda/build_gowin.mk
+++ eda/build_gowin.mk
@@ -1,21 +1,29 @@
 .PHONY: all clean synthesis run deploy
 
-PROGRAMMER_CLI_DIR = $(dir $(shell which programmer_cli))
+DEVICE ?= $(DEVICE_FAMILY)
+PROGRAMMER_CLI_DIR ?= $(dir $(shell which programmer_cli))
+PROGRAMMER_CABLE ?=
 
 all: synthesis
 
 $(BITSTREAM): $(SRCS)
-	gw_sh ./project.tcl
+	mkdir -p build/$(TARGET) && cd build/$(TARGET) && gw_sh ../../project.tcl $(SRC_DIR) $(RTL_DIR) $(TARGET) $(DEVICE_FAMILY) $(DEVICE_PART) 
 
 synthesis: $(BITSTREAM)
 
+scan-cables:
+	cd $(PROGRAMMER_CLI_DIR); ./programmer_cli --scan-cables
+
+scan:
+	cd $(PROGRAMMER_CLI_DIR); ./programmer_cli --scan
+
 run: $(BITSTREAM)
 	if lsmod | grep ftdi_sio; then sudo modprobe -r ftdi_sio; fi
-	cd $(PROGRAMMER_CLI_DIR); ./programmer_cli --device $(DEVICE) --run 2 --fsFile $(abspath $(BITSTREAM))
+	cd $(PROGRAMMER_CLI_DIR); ./programmer_cli $(PROGRAMMER_CABLE) --device $(DEVICE) --run 2 --fsFile $(abspath $(BITSTREAM))
 
 deploy: $(BITSTREAM)
 	if lsmod | grep ftdi_sio; then sudo modprobe -r ftdi_sio; fi
-	cd $(PROGRAMMER_CLI_DIR); ./programmer_cli --device $(DEVICE) --run 6 --fsFile $(abspath $(BITSTREAM))
+	cd $(PROGRAMMER_CLI_DIR); ./programmer_cli $(PROGRAMMER_CABLE) --device $(DEVICE) --run 6 --fsFile $(abspath $(BITSTREAM))
 
 clean:
 	-@$(RM) -r impl
