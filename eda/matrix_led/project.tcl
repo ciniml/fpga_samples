7c304537205713639cceafe755c44f3f8b537cc4
Kenta IDA <fuga@fugafuga.org>
2022-05-11 04:23:24 +0900

    Add matrix led example for TangNano9K with ComProc board rev.2

---
+++ eda/matrix_led/project.tcl
@@ -0,0 +1,18 @@
+set SRC_DIR       [lindex $argv 0]
+set RTL_DIR       [lindex $argv 1]
+set TARGET        [lindex $argv 2]
+set DEVICE_FAMILY [lindex $argv 3]
+set DEVICE_PART   [lindex $argv 4]
+
+set_option -output_base_name matrix_led
+set_device -name $DEVICE_FAMILY $DEVICE_PART
+
+set_option -verilog_std sysv2017
+set_option -vhdl_std vhd2008
+set_option -print_all_synthesis_warning 1
+set_option -use_sspi_as_gpio 1
+
+add_file -type verilog [file normalize ${SRC_DIR}/top.sv]
+add_file -type cst [file normalize ${SRC_DIR}/pins.cst]
+
+run all
