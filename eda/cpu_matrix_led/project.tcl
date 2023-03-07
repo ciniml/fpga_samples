893d159af25243b62b78914e16e51ef88c23bb87
Kenta IDA <fuga@fugafuga.org>
2023-03-08 05:24:43 +0900

    Add Tang Primer 20K Pmod sample.

--- eda/cpu_matrix_led/project.tcl
+++ eda/cpu_matrix_led/project.tcl
@@ -12,6 +12,8 @@ set_option -verilog_std sysv2017
 set_option -vhdl_std vhd2008
 set_option -print_all_synthesis_warning 1
 set_option -use_sspi_as_gpio 1
+set_option -use_done_as_gpio 1
+set_option -use_ready_as_gpio 1
 
 add_file -type verilog [file normalize ${RTL_DIR}/../external/picorv32/picorv32.v]
 add_file -type verilog [file normalize ${SRC_DIR}/top.sv]
