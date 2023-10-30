9b642bc33d78719635badfeb724fa5ff951b19a0
Kenta IDA <fuga@fugafuga.org>
2023-10-31 01:52:36 +0900

    Fix udpaudiosend to fill the buffer to full from stdin to avoid short UDP packet.

--- util/udpaudiosend/src/main.rs
+++ util/udpaudiosend/src/main.rs
@@ -20,7 +20,7 @@ fn main() -> anyhow::Result<()> {
     let socket = UdpSocket::bind(&args.bind_to)?;
     socket.set_read_timeout(Some(Duration::from_millis(10)))?;
     let (sender, receiver) = std::sync::mpsc::channel();
-    let (mut buffer_return, buffer_acquire) = std::sync::mpsc::channel();
+    let (buffer_return, buffer_acquire) = std::sync::mpsc::channel();
     for _ in 0..128 {
         buffer_return.send(vec![0; 1024]).unwrap();
     }
@@ -46,25 +46,60 @@ fn main() -> anyhow::Result<()> {
     } else {
         std::thread::spawn(move || loop {
             let mut buf = buffer_acquire.recv().unwrap();
-            let bytes_read = std::io::stdin().read(&mut buf).unwrap();
+            let mut bytes_read = 0;
+            while bytes_read < buf.len() {
+                bytes_read += std::io::stdin().read(&mut buf[bytes_read..]).unwrap();
+            }
             buf.truncate(bytes_read);
             sender.send(buf).unwrap();
         });
     }
 
+    struct BufferUsage {
+        pub remaining: u16,
+        pub maximum: u16,
+    }
+
+    let mut buffer_usage: Option<BufferUsage> = None;
+    const BUFFER_SIZE: u16 = 1024/4;
     loop {
-        let packet = receiver.recv().unwrap();
-        if let Err(error) = socket.send_to(&packet, &args.destination) {
-            log::error!("pattern send error: {}", error);
+        loop {
+            let (new_buffer_usage, need_send, exit) = match buffer_usage {
+                None => (None, true, true),
+                Some(usage) if usage.maximum - usage.remaining < BUFFER_SIZE => (Some(usage), false, true),
+                Some(usage) => {
+                    (Some(BufferUsage {
+                        remaining: usage.remaining + BUFFER_SIZE,
+                        maximum: usage.maximum,
+                    }), true, usage.maximum - usage.remaining - BUFFER_SIZE >= BUFFER_SIZE)
+                },
+            };
+            buffer_usage = new_buffer_usage;
+            if need_send {
+                let packet = receiver.recv().unwrap();
+                if let Err(error) = socket.send_to(&packet, &args.destination) {
+                    log::error!("pattern send error: {}", error);
+                }
+                buffer_return.send(packet).unwrap();
+                if let Some(usage) = &buffer_usage {
+                    log::debug!("{}/{}", usage.remaining, usage.maximum);
+                }
+            }
+            if exit {
+                break;
+            }
         }
-        buffer_return.send(packet).unwrap();
         let mut buf = [0u8; 5];
         if let Ok((size, from)) = socket.recv_from(&mut buf) {
             if size == 5 {
                 let remaining = u16::from_be_bytes([buf[1], buf[2]]);
                 let maximum = u16::from_be_bytes([buf[3], buf[4]]);
                 log::debug!("{}/{} from {}", remaining, maximum, from);
+                buffer_usage = Some(BufferUsage { remaining, maximum })
             }
+        }
+        else {
+            buffer_usage = None;
         }
     }
 }
